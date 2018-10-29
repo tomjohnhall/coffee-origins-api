1eaaf8a9c160c56e7ef8cbbd3ec279dc134d1c6f
TJH <thomasjohnhall@gmail.com>
2018-10-29 21:21:37 +0000

    refresh

--- coffee/coffee/settings.py
+++ coffee/coffee/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
 }
 
 import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(conn_max_age=500, ssl_require=True)
 
 
 
