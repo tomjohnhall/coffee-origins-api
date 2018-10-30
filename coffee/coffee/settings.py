3b1dc501617ff89f8cd2eb6ba15a5fd3f4e8f6cf
TJH <thomasjohnhall@gmail.com>
2018-10-30 17:30:46 +0000

    sendgrid settings

--- coffee/coffee/settings.py
+++ coffee/coffee/settings.py
@@ -150,10 +150,10 @@ SITE_ID = 1
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = os.environ.get('MAILGUN_SMTP_SERVER')
-EMAIL_PORT = os.environ.get('MAILGUN_SMTP_PORT')
-EMAIL_HOST_USER = os.environ.get('MAILGUN_SMTP_LOGIN')
-EMAIL_HOST_PASSWORD = os.environ.get('MAILGUN_SMTP_PASSWORD')
+EMAIL_HOST = os.environ.get('SENDGRID_SERVER')
+EMAIL_PORT = os.environ.get('SENDGRID_PORT')
+EMAIL_HOST_USER = os.environ.get('SENDGRID_USERNAME')
+EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD')
 EMAIL_USE_TLS = True
 
 REST_FRAMEWORK = {
